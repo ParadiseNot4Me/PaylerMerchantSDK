d8140669246abe3e23034f03bb93fbbadb4c9f2e
Иван Григорьев <ig@poloniumarts.com>
2015-05-04 23:39:35 +0300

    [add] interface

---
+++ PaylerMerchantSDK/PLRTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  PLRTableViewController.h
+//  PaylerMerchantSDK
+//
+//  Created by Иван Григорьев on 04.05.15.
+//  Copyright (c) 2015 poloniumarts. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PLRTableViewController : UITableViewController
+
+@end
---
+++ PaylerMerchantSDK/PLRTableViewController.m
@@ -0,0 +1,26 @@
+//
+//  PLRTableViewController.m
+//  PaylerMerchantSDK
+//
+//  Created by Иван Григорьев on 04.05.15.
+//  Copyright (c) 2015 poloniumarts. All rights reserved.
+//
+
+#import "PLRTableViewController.h"
+#import "PLRViewController.h"
+
+static NSString *const OneStepSegueIdentifier = @"OneStepSegue";
+static NSString *const TwoStepSegueIdentifier = @"TwoStepSegue";
+
+@implementation PLRTableViewController
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    PLRViewController *controller = segue.destinationViewController;
+    if ([segue.identifier isEqualToString:OneStepSegueIdentifier]) {
+       
+    } else if ([segue.identifier isEqualToString:TwoStepSegueIdentifier]) {
+      
+    }
+}
+
+@end
