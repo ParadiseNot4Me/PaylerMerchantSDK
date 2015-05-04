1cf77fd2e0b8796618d9e876ebdaa757956a39e6
Иван Григорьев <ig@poloniumarts.com>
2015-05-04 20:03:47 +0300

    Initial Commit

---
+++ PaylerMerchantSDK/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PaylerMerchantSDK
+//
+//  Created by Иван Григорьев on 04.05.15.
+//  Copyright (c) 2015 poloniumarts. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PaylerMerchantSDK/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PaylerMerchantSDK
+//
+//  Created by Иван Григорьев on 04.05.15.
+//  Copyright (c) 2015 poloniumarts. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
